71262c6a0ff1c6edc3e4693c071e7283b2611017
user <jenn@ubuntu.ubuntu-domain>
2010-04-19 12:12:58 -0700

    initial design

---
+++ public/javascripts/fade_effect.js
@@ -0,0 +1,32 @@
+var Header = {
+ addFade : function(selector){
+  $('span.fake-hover> span').css("display", "none").prependTo($(selector));
+  $(selector+" a").bind("mouseenter",function(){
+    $(selector+" .fake-hover").fadeIn("slow");
+  });
+  $(selector+" a").bind("mouseleave",function(){
+    $(selector+" .fake-hover").fadeOut("slow");
+    });
+  }
+};
+
+$(document).ready(function() {
+    $('img').each(function() {
+        $(this).hover(function() {
+            $(this).stop().animate({ opacity: 1.0 }, 500);
+        },
+       function() {
+           $(this).stop().animate({ opacity: 0.5 }, 500);
+       });
+    });
+});
+
+$(document).ready(function(){
+  $(document).ready(function () {
+    $('#page_effect').fadeIn(1500);
+  });
+
+  $(function(){
+    Header.addFade("#head_image");
+  });
+});
