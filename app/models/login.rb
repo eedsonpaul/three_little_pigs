9c25aee3f266e070a366def32d4a33e88632417b
user <jenn@ubuntu.ubuntu-domain>
2010-04-23 21:25:04 -0700

    started dashboard

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,11 +1,11 @@
 class Login < ActiveRecord::Base
-	
-	def self.duplicate(id)
-		u = User.find_by_twitter_id(id)
-		if u
-			return true
-		else return false
-		end		
-	end
+
+  def self.duplicate(id)
+	  u = User.find_by_twitter_id(id)
+	  if u
+		  return true
+	  else return false
+	  end		
+  end
 	
 end
