d920e2222c867110e9e3beffdb4a6d985fd1457b
user <jenn@ubuntu.ubuntu-domain>
2010-04-27 01:43:29 -0700

    session views

--- public/javascripts/fade_effect.js
+++ public/javascripts/fade_effect.js
@@ -40,4 +40,31 @@ $(document).ready(function(){
   $(function(){
     Header.addFade("#head_image");
   });
+
+  $(function() {
+    $(window).scroll(function(){
+      var scrollTop = $(window).scrollTop();
+      if(scrollTop != 0)
+        $('#nav').stop().animate({'opacity':'0.2'},400);
+      else	
+        $('#nav').stop().animate({'opacity':'1'},400);
+      });
+
+      $('#nav').hover(
+        function (e) {
+          var scrollTop = $(window).scrollTop();
+          if(scrollTop != 0){
+            $('#nav').stop().animate({'opacity':'1'},400);
+            }
+        },
+
+        function (e) {
+          var scrollTop = $(window).scrollTop();
+          if(scrollTop != 0){
+            $('#nav').stop().animate({'opacity':'0.2'},400);
+            }
+        }
+      );
+  });
 });
+
