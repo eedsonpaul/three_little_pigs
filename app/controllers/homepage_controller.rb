3f6763a0b912cd35b8b8b2884ac6054189ef4dda
user <jenn@ubuntu.ubuntu-domain>
2010-04-28 19:40:05 -0700

    fixed layout

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -13,13 +13,13 @@ class HomepageController < ApplicationController
   	@user = session[:user]
   	if request.post?
 			if Login.duplicate_token(params[:users][:token]) == true
-		  	logger.info ("true")
+		  	logger.info("true")
 		  	if @user.token == params[:users][:token]
 		  		redirect_to dash_path
 		  	else redirect_to home_path
 		  	end
 		  else
-		  	logger.info ("false")
+		  	logger.info("false")
 		  	@user.update_attributes(:token => params[:users][:token])
   			redirect_to dash_path
 		  end
--- public/javascripts/fade_effect.js
+++ public/javascripts/fade_effect.js
@@ -45,23 +45,23 @@ $(document).ready(function(){
     $(window).scroll(function(){
       var scrollTop = $(window).scrollTop();
       if(scrollTop != 0)
-        $('#nav').stop().animate({'opacity':'0.2'},400);
+        $('#head-board').stop().animate({'opacity':'0.2'},400);
       else	
-        $('#nav').stop().animate({'opacity':'1'},400);
+        $('#head-board').stop().animate({'opacity':'1'},400);
       });
 
-      $('#nav').hover(
+      $('#hear-board').hover(
         function (e) {
           var scrollTop = $(window).scrollTop();
           if(scrollTop != 0){
-            $('#nav').stop().animate({'opacity':'1'},400);
+            $('#head-board').stop().animate({'opacity':'1'},400);
             }
         },
 
         function (e) {
           var scrollTop = $(window).scrollTop();
           if(scrollTop != 0){
-            $('#nav').stop().animate({'opacity':'0.2'},400);
+            $('#head-board').stop().animate({'opacity':'0.2'},400);
             }
         }
       );
