1dfb6c9f3e281fc55ee8a0845c7defcaa937d7ab
stefanymarie <stefanymarie@stefany-xyz.(none)>
2010-04-30 16:57:46 +0800

    sortable but not save-able, code cleaned-up

--- app/models/login.rb
+++ app/models/login.rb
@@ -56,4 +56,18 @@ class Login < ActiveRecord::Base
 		@return_val = [@project_xmls,@labels, @project_stories]
 	end
 	
+	def self.find_labels (xmls, label)
+	  ret = []
+	  (xmls/'story').each do |s|
+	    if s.innerHTML != nil
+	      if s.at('labels')
+	        ret << s if s.at('labels').innerHTML == label or s.at('story_type').innerHTML == "release"
+	      else
+	        ret << s if label == "Other Activities" or  s.at('story_type').innerHTML == "release"
+	      end
+	    end
+	  end
+	  @return_val = ret
+	end
+	
 end
