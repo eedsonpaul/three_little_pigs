1e7fee40c0331b3e34adf2fc8a621e9ced8c8b06
stefanymarie <stefanymarie@stefany-xyz.(none)>
2010-05-28 16:09:56 +0800

    minor changes: limitations of sortability

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,17 +1,31 @@
 class Tracker < ActiveRecord::Base
 
 	def self.find_labels (xmls, label)
-	  ret = []
+	  @unstarted = []
+	  @started = []
+	  @accepted = []
 	  (xmls/'story').each do |s|
 	    if s.innerHTML != nil
 	      if s.at('labels') != nil
-	        ret << s if s.at('labels').innerHTML == label or s.at('story_type').innerHTML == "release"
+	        if s.at('current_state').innerHTML == 'unstarted'
+	          @unstarted << s if s.at('labels').innerHTML == label or s.at('story_type').innerHTML == "release"
+	        elsif s.at('current_state').innerHTML == 'accepted'
+	          @accepted << s if s.at('labels').innerHTML == label #or s.at('story_type').innerHTML == "release"
+	        else
+	          @started << s if s.at('labels').innerHTML == label #or s.at('story_type').innerHTML == "release"
+	        end
 	      else
-	        ret << s if label == "Other Activities" or  s.at('story_type').innerHTML == "release"
+	        if s.at('current_state').innerHTML == 'unstarted'
+	          @unstarted << s if label == "Other Activities" or  s.at('story_type').innerHTML == "release"
+	        elsif s.at('current_state').innerHTML == 'accepted'
+	          @accepted << s if label == "Other Activities"# or  s.at('story_type').innerHTML == "release"
+	        else
+	          @started << s if label == "Other Activities" # or  s.at('story_type').innerHTML == "release"
+	        end
 	      end
 	    end
 	  end
-	  @return_val = ret
+	  @return_val = [@accepted, @started, @unstarted]
 	end
 	
 end
